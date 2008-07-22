ca30c1d97c299c3af9fef2c9de93264509b420f3
Jonas Bon& <jonas@jonasboner.com>
2008-07-22 16:28:21 +0200

    added implicit defs for all primitive types <- -> index

--- mnesia/src/main/scala/scala/actors/mnesia/Index.scala
+++ mnesia/src/main/scala/scala/actors/mnesia/Index.scala
@@ -4,11 +4,39 @@
 
 package scala.actors.mnesia
 
+/**
+ * Implicit definitions converting Scala primitive types to Indexes and vice versa.
+ * <p/>
+ * Import into user classes in order to avoid explicit Index management: 
+ * <pre>
+ * import scala.actors.mnesia.Index._
+ * </pre>
+ *
+ * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
+ */
 object Index {
-  implicit def stringToIndex(str: String): StringIndex = StringIndex(str)
+  implicit def stringToIndex(value: String): StringIndex = StringIndex(value)
   implicit def indexToString(index: StringIndex): String = index.value
-}
 
+  implicit def shortToIndex(value: Short): ShortIndex = ShortIndex(value)
+  implicit def indexToShort(index: ShortIndex): Short = index.value
+
+  implicit def intToIndex(value: Int): IntIndex = IntIndex(value)
+  implicit def indexToInt(index: IntIndex): Int = index.value
+
+  implicit def longToIndex(value: Long): LongIndex = LongIndex(value)
+  implicit def indexToLong(index: LongIndex): Long = index.value
+
+  implicit def floatToIndex(value: Float): FloatIndex = FloatIndex(value)
+  implicit def indexToFloat(index: FloatIndex): Float = index.value
+
+  implicit def doubleToIndex(value: Double): DoubleIndex = DoubleIndex(value)
+  implicit def indexToDouble(index: DoubleIndex): Double = index.value
+
+  implicit def charToIndex(value: Char): CharIndex = CharIndex(value)
+  implicit def indexToChar(index: CharIndex): Char = index.value
+
+}
 
 /**
  * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
@@ -24,6 +52,14 @@ abstract case class Index extends Ordered[Index] {
  */
 case class StringIndex(override val value: String) extends Index {
   type T = String
+  override def hashCode: Int = HashCode.hash(HashCode.SEED, value)
+}
+
+/**
+ * @author <a href="http://jonasboner.com">Jonas Bon&#233;r</a>
+ */
+case class ShortIndex(override val value: Short) extends Index {
+  type T = Short
   override def hashCode: Int = HashCode.hash(HashCode.SEED, value)
 }
 
