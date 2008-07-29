c5d2962e4fbb2cc1bcaaad68f28944c7dbee0aed
Rich Dougherty <rich@rd.gen.nz>
2008-07-29 16:31:38 +1200

    Reverted to bounded-length reads. (Realised that available data may be very large (eg for memory-mapped files).

--- io/src/main/scala/scala/actors/io/RichReadableByteChannel.scala
+++ io/src/main/scala/scala/actors/io/RichReadableByteChannel.scala
@@ -12,84 +12,60 @@ trait RichReadableByteChannel {
   val channel: SelectableChannel with ReadableByteChannel
   val richSelector: RichSelector
 
-  protected[this] val bufferLength: Int
-
-  /**
-   * "Copies" a ByteBuffer into a Binary, consuming the buffer in the
-   * process.  A new, equal-sized ByteBuffer is returned along with
-   * the Binary. Returned ByteBuffers have a position of zero, a limit
-   * set to the capacity and no mark - equivalent to calling clear.
-   */
-  private[this] def copy(buffer: ByteBuffer): (ByteBuffer, Binary) = {
-    if (buffer.hasArray && buffer.remaining == buffer.capacity) {
-      // Consume the ByteBuffer and create a new one.
-      //println("Wrapping ByteBuffer directly in Binary.")
-      val newBuffer = ByteBuffer.allocate(buffer.capacity)
-      val binary = Binary.fromSeq(buffer.array, buffer.arrayOffset, buffer.capacity, false)
-      (newBuffer, binary)
-    } else {
-      // Copy the ByteBuffer's contents.
-      //println("Copying ByteBuffer contents into Binary.")
-      val array = new Array[Byte](buffer.remaining)
-      buffer.get(array)
-      val binary = Binary.fromSeq(array, 0, array.length, false)
-      buffer.clear
-      (buffer, binary)
-    }
-  }
+  protected[this] def defaultReadLength: Int = 256
 
   // returns zero-length Binary when reaches end
-  def asyncRead(k: Cont[Binary]): Nothing = {
+  def asyncRead(maxLength: Int)(k: Cont[Binary]): Nothing = {
     import k.exceptionHandler
-    def tryRead(buffer: ByteBuffer, accum: Binary): Nothing = {
-      buffer.clear
+    val buffer = ByteBuffer.allocate(maxLength)
+    def tryRead: Nothing = {
       channel.read(buffer) match {
-        case -1 => k(accum)
-        case 0 if accum.isEmpty => {
+        case -1 => k(Binary.empty)
+        case 0 => {
           // No data available yet: set callback.
           //println("No bytes for reading: awaiting callback.") 
-          richSelector.register(channel, RichSelector.Read) { () => tryRead(buffer, accum) }
+          richSelector.register(channel, RichSelector.Read) { () => tryRead }
           Actor.exit
         }
-        case 0 => {
-          // Reached end of available data: sending accum.
-          //println("No bytes for reading: ending read.")
-          k(accum)
-        }
         case length => {
-          //println("Read "+length+" bytes.")
-          buffer.flip
-          val (newBuffer, binary) = copy(buffer)
-          tryRead(newBuffer, accum ++ binary)
+          // XXX: Could sometimes use array if length < maxLength - would save a copy.
+          if (length == maxLength && buffer.hasArray) {
+            k(Binary.fromSeq(buffer.array, buffer.arrayOffset, buffer.capacity, false))
+          } else {
+            buffer.flip
+            val array = new Array[Byte](buffer.remaining)
+            buffer.get(array)
+            k(Binary.fromSeq(array, 0, array.length, false))
+          }
         }
       }
     }
-    tryRead(ByteBuffer.allocate(bufferLength), Binary.empty)
+    tryRead
   }
 
   // warn about memory usage!
   def asyncReadAll(k: Cont[Binary]): Nothing = {
     import k.exceptionHandler
     // XXX: Replace with asyncFold.
-    def asyncReadAll0(accum: Binary): Nothing = {
-      asyncRead { binary: Binary =>
+    def readAllLoop(accum: Binary): Nothing = {
+      asyncRead(defaultReadLength) { binary: Binary =>
         if (binary.isEmpty) {
           k(accum)
         } else {
-          asyncReadAll0(accum ++ binary)
+          readAllLoop(accum ++ binary)
         }
       }
     }
-    asyncReadAll0(Binary.empty)
+    readAllLoop(Binary.empty)
   }
 
-  def asyncReadStream(k: Cont[AsyncStream[Binary]]): Nothing = {
+  def asyncReadStream(maxBinaryLength: Int)(k: Cont[AsyncStream[Binary]]): Nothing = {
     import k.exceptionHandler
-    asyncRead { binary: Binary =>
+    asyncRead(maxBinaryLength) { binary: Binary =>
       if (binary.isEmpty) {
         k(AsyncStream.empty)
       } else {
-        k(AsyncStream.cons(binary, asyncReadStream _))
+        k(AsyncStream.cons(binary, asyncReadStream(maxBinaryLength) _))
       }
     }
   }
