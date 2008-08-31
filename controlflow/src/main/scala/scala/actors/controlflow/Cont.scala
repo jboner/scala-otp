95dddc1d670b204f62ae5fba13c449ee1e0f8c08
Rich Dougherty <rich@rd.gen.nz>
2008-08-31 21:35:24 +1200

    Fixed typo in docs.

--- controlflow/src/main/scala/scala/actors/controlflow/Cont.scala
+++ controlflow/src/main/scala/scala/actors/controlflow/Cont.scala
@@ -9,7 +9,7 @@ trait Cont[-R] {
 
   /**
    * Continue with this continuation, supplying the given value to the future
-   * computation. This method never executes normally; it will always throw an
+   * computation. This method never exits normally; it will always throw an
    * exception. These exceptions are used to manage the control flow, so should
    * never be caught by the calling code. Calling code can use
    * <code>ControlFlow.isControlFlowThrowable()</code> to determine whether or
