b2c514613cd3cf5cbeec75d71e0253f819e2d98f
Rich Dougherty <rich@rd.gen.nz>
2008-08-31 21:36:08 +1200

    Made AsyncFuture covariant.

--- controlflow/src/main/scala/scala/actors/controlflow/concurrent/AsyncFuture.scala
+++ controlflow/src/main/scala/scala/actors/controlflow/concurrent/AsyncFuture.scala
@@ -3,7 +3,7 @@ package scala.actors.controlflow.concurrent
 import scala.actors.controlflow.ControlFlow._
 import scala.collection.immutable.Queue
 
-trait AsyncFuture[A] extends AsyncFunction0[A] {
+trait AsyncFuture[+A] extends AsyncFunction0[A] {
   
   def isSet: Boolean
   
