8eeddfea06d01de0d04157056db165733adcd796
Jonas Boner <jonas@jonasboner.com>
2009-02-17 19:48:04 +0100

    fixed broken logging

--- behavior/src/main/scala/scala/actors/behavior/Helpers.scala
+++ behavior/src/main/scala/scala/actors/behavior/Helpers.scala
@@ -92,7 +92,7 @@ object Helpers extends Logging {
         def receiveWithin(timeout: Int): Option[A] = value match {
           case None => ch.receiveWithin(timeout) {
             case TIMEOUT =>
-              log.debug("Future timed out while waiting for actor: {}", a)
+              log.debug("Future timed out while waiting for actor: %s", a)
               None
             case a: A =>
               value = Some(a)
